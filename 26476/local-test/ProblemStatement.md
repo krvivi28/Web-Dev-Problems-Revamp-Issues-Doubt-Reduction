832c936ddc94408dbcd067716b50713e88647108
vivek <krvivi28@gmail.com>
2023-12-19 01:04:31 +0530

    changed problem id 26476

---
+++ 26476/local-test/ProblemStatement.md
@@ -0,0 +1,14 @@
+# Implement Update Functionality for User Data
+
+You are tasked with enhancing the existing Node.js application's functionality by implementing an update feature for user data. The application provides a POST route at ('/') that utilizes the 'updateUser' controller to update an existing user in the 'users' array, as specified in the user model. The user interface for this update functionality is facilitated through the 'update-form.ejs' view.
+
+# Objective
+
+1. Implement the 'updateUsers' Model:
+
+   Implement the 'updateUsers' model, which is utilized by the 'updateUser' controller to update existing user data.
+   This function should be designed to efficiently update a user's details within the 'users' array based on their unique identifier ('id').
+   Upon successful update, the function should return the updated user details to the controller.
+   If the specified user is not present in the 'users' array, the function should gracefully return false to the controller.
+
+# Output
---
+++ 26476/local-test/app.test.js
@@ -0,0 +1,29 @@
+import server from "./index.js";
+import request from "supertest";
+import { updateUser } from "./user.controller.js";
+import { users } from "./user.model.js";
+const initialUsersLength = users.length;
+
+describe("POST /", () => {
+  it("should update users array for a valid 'id' and render update-form view with updated user data", async () => {
+    const response = await request(server).post("/").type("form").send({
+      id: 1,
+      name: "John Doe",
+      email: "john.doe@example.com",
+      image: "https://example.com/image.jpg",
+    });
+
+    expect(response.text).toContain("John Doe");
+    expect(response.text).toContain("john.doe@example.com");
+  });
+  // it("should return false to the controller for invalid 'id' and render update-form view with error message", async () => {
+  //   const response = await request(server).post("/").type("form").send({
+  //     id: 2,
+  //     name: "John Doe",
+  //     email: "john.doe@example.com",
+  //     image: "https://example.com/image.jpg",
+  //   });
+
+  //   expect(response.status).toBe(400);
+  // });
+});
